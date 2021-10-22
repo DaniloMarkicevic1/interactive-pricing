e53bd249e33d02db1ebae3980f499c2737c9ea26
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2021-10-22 09:22:22 +0200

    mostlyFinished

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,8 @@ const monthly = document.querySelector('.outer');
 const circle = document.querySelector('.inner');
 const slider = document.querySelector('#slider');
 const body = document.querySelector('body');
-
 const cost = document.querySelector('.cost');
+const views = document.querySelector('.views');
 
 monthly.addEventListener('click', (e) => {
     console.log(e.target.classList[1]);
@@ -15,11 +15,31 @@ monthly.addEventListener('click', (e) => {
         monthly.classList.remove('activeBg');
     }
 });
+
 function myFunction() {
     const x = document.querySelector('#slider');
     const defaultVal = x.defaultValue;
     const currentVal = x.value;
-    console.log(x);
-    console.log(defaultVal);
-    console.log(currentVal);
+    switch (currentVal) {
+        case '0':
+            views.innerText = '10K';
+            cost.innerText = '$8';
+            break;
+        case '1':
+            views.innerText = '50K';
+            cost.innerText = '$12';
+            break;
+        case '2':
+            views.innerText = '100K';
+            cost.innerText = '$16';
+            break;
+        case '3':
+            views.innerText = '500k';
+            cost.innerText = '$24';
+            break;
+        case '4':
+            views.innerText = '1M';
+            cost.innerText = '$36';
+            break;
+    }
 }
